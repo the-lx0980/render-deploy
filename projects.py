1a9035c8144339e1cecbf1472a9d9b16daee61cd
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-06 22:38:00 +0530

    Create projects.py

---
+++ projects.py
@@ -0,0 +1,10 @@
+PROJECTS = {
+    "MyApp1": {
+        "app_url": "https://myapp1.onrender.com",
+        "deploy_url": "https://api.render.com/deploy/somehook1"
+    },
+    "MyApp2": {
+        "app_url": "https://myapp2.onrender.com",
+        "deploy_url": "https://api.render.com/deploy/somehook2"
+    },
+}
